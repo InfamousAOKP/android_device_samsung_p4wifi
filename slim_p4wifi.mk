4bd0ebb5d43eac7b1e3e8151d8a5f51300e6efba
Kasper Hettinga <kasper.hettinga@gmail.com>
2013-11-03 17:36:07 +0100

    Update to SLIM ROM

--- slim_p4wifi.mk
+++ slim_p4wifi.mk
@@ -1,13 +1,13 @@
 # Inherit device configuration for p4wifi.
 $(call inherit-product, device/samsung/p4wifi/p4wifi.mk)
 
-# Inherit some common cyanogenmod stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common slim stuff.
+$(call inherit-product, vendor/slim/config/common.mk)
 
 #
 # Setup device specific product configuration.
 #
-PRODUCT_NAME := omni_p4wifi
+PRODUCT_NAME := slim_p4wifi
 PRODUCT_BRAND := samsung
 PRODUCT_DEVICE := p4wifi
 PRODUCT_MODEL := GT-P7510
